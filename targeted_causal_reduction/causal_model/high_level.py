1d9e5edc8e3f60b6383457dc82ec54183e7d64e1
sermej <sermej@amazon.com>
2024-08-02 16:05:06 +0200

    Changed name of macro scm in high level model and included the learnable parameters of the noise

--- targeted_causal_reduction/causal_model/high_level.py
+++ targeted_causal_reduction/causal_model/high_level.py
@@ -14,7 +14,7 @@ class HighLevelCausalModel(nn.Module):
         The mean of the unintervened macro variables 'z' of shape (n_vars,).
     sigma_z : nn.Parameter
         The standard deviation of the unintervened macro variables 'z' of shape (n_vars,).
-    linear_macro_scm : nn.Module
+    linear_macro_solution : nn.Module
         The linear layer to model the causal mechanism. This is equivalent to (I-A)^(-1)
 
     mu_z, sigma_z and the weights of the linear_macro_scm are learnable parameters.
@@ -24,12 +24,15 @@ class HighLevelCausalModel(nn.Module):
         super().__init__()
         self.n_vars = n_vars
 
+        # TODO: in our case we don't need to learn these parameters
+        #   but only the covariance of the noise variable Cov(N_hat),
+        #   so we might be able to abstract the parameters that need 
+        #   to be learned at the macro level.
         # register parameters for mu_z, sigma_z
-        self.mu_z = nn.Parameter(torch.zeros(n_vars))
-        self.sigma_z = nn.Parameter(torch.ones(n_vars))
-        # self.macro_model = nn.Parameter(torch.ones(1)) # TODO: ask Armin
+        self.mu_N = nn.Parameter(torch.zeros(n_vars))
+        self.cov_N = nn.Parameter(torch.ones(n_vars))
 
-        self.linear_macro_scm = nn.Sequential(nn.Linear(n_vars, 1))
+        self.linear_macro_solution = nn.Sequential(nn.Linear(n_vars, 1))
 
     def causal_mechanism(self, N):
-        return self.linear_macro_scm(N)
+        return self.linear_macro_solution(N)
